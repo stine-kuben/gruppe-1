29537c7bd3bf5c594b02d3bb8b05e6f6e92fc014
JakobGit <jakob.seljelid@gmail.com>
2025-11-12 09:34:49 +0100

    La til erLik funksjon

--- telling.py
+++ telling.py
@@ -3,3 +3,12 @@ def summertall(tall1, tall2):
 summertall(6, 7)
 
 #Kommentar
+
+def erLik(tall1, tall2, tall3):
+    if tall1 == tall2 and tall2 == tall3:
+        return True
+    else:
+        return False
+    
+print(erLik(5, 5, 5))
+print(erLik(5, 3, 5))
