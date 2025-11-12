28d08dd2cefb576a55c368f2f9f17671be59d3cf
yokha001 <khelyounis123@gmail.com>
2025-11-12 09:47:02 +0100

    oppgave 1

--- telling.py
+++ telling.py
@@ -1,3 +1,7 @@
 # Oppgave 1:
-tekst = "alkjsdkemc"
-
+def tell_tegn (tegn, tekst):
+    teller = 0
+    for i in tekst: 
+        if i == tegn:
+            teller += 1
+    return teller
--- test_telling.py
+++ test_telling.py
@@ -1,1 +1,3 @@
-
+from telling import *
+#Oppgave 1
+print(tell_tegn("b", "bananer"))
